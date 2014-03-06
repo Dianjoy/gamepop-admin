f9b50cdc28a48939ff0a98744ccfabcd259b1dab
Meathill <meathill@gmail.com>
2014-03-06 18:46:32 +0800

    init

---
+++ inc/pdo_sample.php
@@ -0,0 +1,16 @@
+<?php
+$db = array(
+  'db'=>'mysql',
+  'host'=>'host',
+  'port'=>'port',
+  'name'=>'db'
+);
+$user = 'user';
+$password = 'password';
+$options = array(
+  PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES \'UTF8\'"
+);
+return new PDO($db['db'] . ':host=' . $db['host']
+  . ';port=' . $db['port']
+  . ';dbname=' . $db['name'],
+  $user, $password, $options);
