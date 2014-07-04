3deb478ae2d24909133c4a62e45cd6bee2ab1698
Meathill <meathill@gmail.com>
2014-07-04 15:51:17 +0800

    增加抓取周期设置

--- api/game/source.php
+++ api/game/source.php
@@ -22,21 +22,30 @@ function fetch($args) {
   $sources = $fetcher->select(Fetcher::$ALL)
     ->where($conditions)
     ->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($sources as $key => $item) {
+    $sources[$key]['label'] = getLabel($item['min_catch_time']);
+  }
+
 
   Spokesman::say(array(
     'total' => count($sources),
     'list' => $sources,
   ));
 }
-function update($args) {
+function update($args, $attr) {
   $fetcher = new Fetcher();
 
+  unset($attr['label']);
+
   $conditions = Spokesman::extract(true);
-  $result = $fetcher->update($args, Fetcher::SOURCE)
+  $result = $fetcher->update($attr, Fetcher::SOURCE)
     ->where($conditions)
     ->execute();
 
-  Spokesman::judge($result, '修改成功', '修改失败');
+  if (isset($attr['min_catch_time'])) {
+    $attr['label'] = getLabel($attr['min_catch_time']);
+  }
+  Spokesman::judge($result, '修改成功', '修改失败', $attr);
 }
 function create($args, $attr) {
   $fetcher = new Fetcher();
@@ -48,4 +57,33 @@ function create($args, $attr) {
 
   $args = array_merge(array('id' => $result), $args);
   Spokesman::judge($result, '创建成功', '创建失败', $args);
+}
+
+function getLabel($time) {
+  $hours = $time / 3600;
+  switch ($hours) {
+    case 2:
+      return '默认';
+      break;
+
+    case 12:
+      return '半天';
+      break;
+
+    case 24:
+      return '一天';
+      break;
+
+    case 168:
+      return '一周';
+      break;
+
+    case 720:
+      return '一月';
+      break;
+
+    default:
+      return '未知';
+      break;
+  }
 }
