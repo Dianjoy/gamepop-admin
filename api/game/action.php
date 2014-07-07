f80f3a5240b682d52eae7e6a77cbf4f9879b3ca2
Meathill <meathill@gmail.com>
2014-07-07 10:51:27 +0800

    修复合并游戏功能

--- api/game/action.php
+++ api/game/action.php
@@ -12,20 +12,57 @@ include_once '../../inc/session.php';
 
 require_once "../../inc/Spokesman.class.php";
 require_once "../../inc/Game.class.php";
+require_once "../../inc/utils.php";
 $game = new Game();
 
-$args = $_REQUEST;
-$args['os_android'] = (int)in_array(1, $args['platform']);
-$args['os_ios'] = (int)in_array(2, $args['platform']);
-$args['icon_path'] = str_replace('http://r.yxpopo.com/', '', $args['icon_path']);
-unset($args['platform']);
+switch ($_GET['m']) {
+  case 'add':
+    $attr = array_pick($_REQUEST, 'guide_name', 'game_name', 'game_desc');
+    $attr['os_android'] = (int)in_array(1, $_REQUEST['platform']);
+    $attr['os_ios'] = (int)in_array(2, $_REQUEST['platform']);
+    $attr['icon_path'] = str_replace('http://r.yxpopo.com/', '', $_REQUEST['icon_path']);
 
-$check = $game->insert($args)
-  ->execute()
-  ->getResult();
+    $result = $game->insert($attr)
+      ->execute()
+      ->getResult();
 
-$args = array(
-  'go_to_url' => '#/game/profile/' . $args['guide_name']
-);
+    $args = array(
+      'go_to_url' => '#/game/profile/' . $args['guide_name']
+    );
+    $success = '创建成功';
+    $error = '创建失败';
+    break;
 
-Spokesman::judge($check, '创建成功', '创建失败', $args);
+  case 'merge':
+    $from = $_REQUEST['from'];
+    $to = $_REQUEST['to'];
+    // 建立关联
+    require_once "../../inc/Source.class.php";
+    $source = new Source();
+    $result = $source->insert(array(
+        '4399id' => $to,
+        'ptbusid' => $from,
+      ), Source::VS)
+      ->execute()
+      ->getResult();
+    // 导出文章
+    if ($result) {
+      require_once "../../inc/Article.class.php";
+      $article = new Article();
+      $result = $article->update(array(Game::ID => $to), Article::TABLE)
+        ->where(array(Game::ID => $from))
+        ->execute();
+    }
+    // 删掉来源游戏
+    if ($result) {
+      $result = $game->update(array('status' => Game::DELETED), Game::TABLE)
+        ->where(array(Game::ID => $from))
+        ->execute();
+    }
+    $success = '合并成功';
+    $error = '合并失败';
+    break;
+}
+
+
+Spokesman::judge($result, $success, $error, $args);
